ff37530c87761bce08fc8813d277c7cb50f3a9f1
Ashutosh Jha <144075650+ashutoshashu99@users.noreply.github.com>
2024-07-20 12:49:20 +0530

    add Teaminfo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,12 @@
+import Teams from "@/components/team/teams";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main >
-      <h1 className="text-3xl flex items-center justify-center">This is the main block</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fugiat libero id quae nemo laudantium iusto optio debitis error a assumenda doloremque voluptate quisquam sit, ducimus sunt. Perferendis, rem voluptatibus.
-        Hic maxime atque dolorem obcaecati molestiae eius, quas ab, provident distinctio dolor aut nam nulla dolores magnam minus quia excepturi omnis et ducimus esse! Nam accusamus voluptate expedita repudiandae similique.
-        Accusamus cupiditate nemo explicabo, beatae non voluptas quos neque in commodi aliquid praesentium nulla. Unde sapiente iure, beatae officia provident dignissimos earum corporis facere cupiditate voluptatum modi laudantium est! Aut!
-        Nulla molestias nostrum numquam similique possimus recusandae aliquam maxime ut quo accusantium. Doloremque alias fugiat perferendis debitis quasi sint nam quas fuga, eaque sit, rerum libero soluta, dolores at ea.
-        Rem debitis unde magni, dolores tempora veritatis dolor eveniet facere eaque placeat. Accusantium earum eum odit obcaecati! Illum, enim optio, sequi sint maiores iure beatae amet distinctio quasi unde in.
-        Voluptatem eaque quaerat minima, repellat reiciendis magnam dignissimos error repellendus officiis minus facilis quod perspiciatis, corrupti explicabo harum dolor tempora a accusantium velit praesentium modi ut rem! At, eligendi sequi?
-        Minima enim eos, iste quae doloribus quas mollitia laudantium, alias rerum blanditiis, suscipit soluta dignissimos nostrum esse quos corporis aspernatur autem ullam maxime atque quasi amet sint. Possimus, ab fuga.
-        Quibusdam accusantium adipisci rem, facilis cupiditate quis assumenda molestias doloribus nulla magni enim corrupti nemo neque amet totam voluptatem labore dicta! Quia accusantium ipsum id architecto, ex esse odit obcaecati?
-        Quod molestiae quos tempora atque saepe quas veniam ipsa quibusdam repudiandae perferendis, quo, iure eveniet necessitatibus dicta velit dolores recusandae eligendi eos minima? Cumque, a dolores. Alias placeat modi culpa!
-        Tempore rerum sequi quisquam neque amet laborum molestiae error aliquam tenetur! Voluptas error ipsa iure eius, quis ullam officia libero explicabo molestias et? Tenetur est aspernatur ipsa cum eius sunt.
-        Ad mollitia aut dignissimos repellat dolorem animi harum aliquam, et quis porro, ducimus eligendi alias blanditiis qui tempore quibusdam assumenda veniam nesciunt sapiente laudantium quia ratione reiciendis voluptas pariatur. Molestiae?
-        Mollitia maiores magni beatae omnis, cum corrupti maxime blanditiis, quis harum cupiditate nam rerum soluta amet, quaerat natus aspernatur saepe fugiat dolor earum? Unde, consequuntur quo maxime voluptatem nihil aperiam!
-        Minima dolorum possimus facilis deleniti praesentium nesciunt voluptates eligendi velit magnam, temporibus quia atque! Illo modi corrupti tempore dicta, adipisci quia vel, tempora in sit est vero distinctio perferendis recusandae!
-        Magni dignissimos autem alias provident voluptatibus, iusto illo, error ullam nulla, rerum eos molestias eveniet ipsa. Quae, animi voluptates aliquid optio hic impedit delectus architecto at. Accusamus dolore molestias blanditiis!
-        Recusandae dignissimos reiciendis perferendis harum quisquam explicabo ab modi commodi. Ab atque provident sunt. Iste beatae atque omnis inventore nostrum maiores expedita dolores sequi nisi et. Magnam animi fugiat voluptatum?
-        Asperiores repellendus recusandae unde reiciendis autem vel in. Animi, ullam officiis amet fuga aliquam vitae nesciunt laudantium autem, ipsa nemo placeat facere! Illo totam, vero placeat quas iste sunt aliquam.
-        Fugiat id suscipit alias nobis laboriosam cumque quo rem, doloribus, recusandae delectus perspiciatis qui. Commodi rem perferendis perspiciatis libero sapiente doloribus est ad beatae voluptates magni, natus neque quae placeat.
-        Est error, delectus quas inventore voluptas odit corrupti quasi beatae ad provident tempore esse. Rem optio aliquid, voluptatem placeat quis blanditiis quae qui inventore quod! Autem rem numquam vel obcaecati?
-        Quos mollitia, maiores harum cupiditate iste perspiciatis dolor tempora reiciendis enim velit suscipit laborum quidem dolores ullam eius dolore. Reprehenderit maxime mollitia aperiam excepturi optio ex. Laudantium debitis nulla voluptatem!
-        Error, illum placeat sed possimus debitis cumque dolorem voluptate expedita quod? Fugiat blanditiis dignissimos in, debitis, sed, quod porro harum vero dolorum odio aliquid iusto ad saepe perspiciatis magni quis.
-        Labore deleniti atque accusantium, eius ipsam repellendus quidem itaque, ex sequi praesentium aliquid beatae, quae aliquam ipsa harum libero? Ipsa, corporis libero distinctio ad voluptatibus iure a aut. Ex, nulla.
-        Enim culpa distinctio accusamus libero atque quos quibusdam. Officiis, iure quasi minima aperiam magnam atque maxime temporibus et. Impedit eveniet atque autem numquam aliquid, quibusdam earum quis accusantium sequi sapiente.
-        Dicta sapiente at pariatur sit saepe adipisci assumenda facilis dolor, fugiat, esse ad ab quia in? Quibusdam aspernatur consequuntur, optio unde enim quidem eum quia omnis doloribus nulla necessitatibus accusamus.
-        Quia, aliquam. Ipsum reprehenderit in, nemo aspernatur laboriosam, quae doloribus amet a perspiciatis consequuntur numquam error dignissimos dicta, eaque voluptatum rem ad dolor dolore nam omnis. Possimus repellat velit voluptas.
-        Dignissimos doloribus eaque laboriosam numquam dolorem unde voluptatibus sunt, iure veritatis modi, eligendi nesciunt maxime, placeat cum vero a qui est blanditiis? 
-      </p>
+    <h1 className="tex3xl flex items-center justify-center mt-40">This is the main block</h1>
+      <Teams/>
+      
     </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,6 @@
 
 import * as React from "react"
 import Link from "next/link"
-import { cn } from "@/lib/utils"
-import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
 
 
 
