bfc9ddd00acd8312fcaeecc4dd4f3baa5defa21e
Ashutosh Jha <144075650+ashutoshashu99@users.noreply.github.com>
2024-07-21 12:14:51 +0530

    rename files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Teams from "@/components/teams";
 import Services from "@/components/sevices";
 import Project from "@/components/projects";
 import DevStage from "@/components/dev-stage";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/sticky-navbar";
 
 export default function Home() {
   return (
